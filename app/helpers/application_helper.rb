345d9d2f08f971013f12b2448375a7e49c364382
Akshat Sethi <masterakshata@gmail.com>
2021-01-18 20:34:29 +0530

    added session helpers and changed show view to show username

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+    def isLoggedIn?
+        return true if session[:user_id] != nil
+        return false
+    end
+
+    def current_user_id
+        session[:user_id]
+    end
+
+    def current_user
+        if isLoggedIn?
+            return User.find(session[:user_id])
+        else
+            return nil
+        end
+    end
+
 end
