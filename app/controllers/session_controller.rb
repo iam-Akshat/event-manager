fdc9dcbe50e795ed80accaa24702cf7f5cd5bb90
Akshat Sethi <masterakshata@gmail.com>
2021-01-18 20:21:15 +0530

    added session controller and view for login

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,25 @@
+class SessionController < ActionController::Base
+
+    def new
+        @user  = User.new
+        @which_path = login_path
+    end
+
+    def create
+        user  =  User.find_by(username:login_params[:username])
+        if user.nil?
+            redirect_to 'new'
+        else
+            session[:user_id] = user.id
+            redirect_to user_path(user.id)
+        end
+
+    end
+
+
+    private
+    def login_params
+        params.require(:user).permit(:username)
+    end 
+
+end  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :user , only: [:new,:create] do
+  resources :user , only: [:new,:create,:show] do
   end
+  get 'sign_in',to: 'session#new'
+  post 'login', to: 'session#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
