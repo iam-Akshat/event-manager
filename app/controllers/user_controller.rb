54799ccae6a3b5228b05e1fd611f50467c8bd26c
Akshat Sethi <masterakshata@gmail.com>
2021-01-18 19:37:06 +0530

    fixed controller and route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,12 @@
 class UserController < ActionController::Base
     def new
         @user = User.new
-        @which_path = "/"
+        @which_path = user_index_path
     end
 
     def create
         user = User.new(user_params)
+        user.save
         redirect_to new_user_path
     end
 
