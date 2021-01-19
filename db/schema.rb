c55e6db166e7e3f4546a0c2c7790996b434b7bee
Akshat Sethi <masterakshata@gmail.com>
2021-01-19 18:22:08 +0530

    added event model and creator association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_130006) do
+ActiveRecord::Schema.define(version: 2021_01_18_212917) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
