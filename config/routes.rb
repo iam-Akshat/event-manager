38eb5288e217a1363e54e5810d7460269a0b1efe
Akshat Sethi <masterakshata@gmail.com>
2021-01-20 15:06:53 +0530

    added sign_up option and changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-
+  root 'session#new'
   resources :user , only: [:new,:create,:show] do
     resources :event , only: [:new,:create]
   end
   resources :event , only: [:show,:index]
   get 'sign_in',to: 'session#new'
   post 'login', to: 'session#create'
+  get 'sign_up', to: 'user#new'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
