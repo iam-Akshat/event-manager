a2e3617322adbbd5d9cc866096a901b0aa77eda8
Akshat Sethi <masterakshata@gmail.com>
2021-01-22 17:53:02 +0530

    added unit test for user model

---
+++ spec/user_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe User do
+    context "validates required fields" do
+        it "returns false when username is not present" do
+            user = User.new
+            expect(user.valid?).to eq(false)
+        end
+        it "returns true when username is present" do
+            user = User.new
+            user.username = "valid"
+            expect(user.valid?).to eq(true)
+        end
+    end
+end
