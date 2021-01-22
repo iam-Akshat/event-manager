b6ff12a93a5829cf62528d0372065a690e983f70
Akshat Sethi <masterakshata@gmail.com>
2021-01-22 14:41:55 +0530

    added model validations

--- app/models/user_attendance.rb
+++ app/models/user_attendance.rb
@@ -1,4 +1,7 @@
 class UserAttendance < ApplicationRecord
+
+  validates :attendee,:attended_event,presence: true
+
   belongs_to :attendee, class_name: 'User'
   belongs_to :attended_event, class_name: 'Event'
 end
