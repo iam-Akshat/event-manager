d910fbf09d6ade6c1e01adf8d5efaa1b817187f4
Akshat Sethi <masterakshata@gmail.com>
2021-01-26 03:39:30 +0530

    add test for root page,login and event_index path

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,7 +7,7 @@ class SessionController < ApplicationController
   def create
     user = User.find_by(username: login_params[:username])
     if user.nil?
-      redirect_to 'new'
+      redirect_to root_path
     else
       session[:user_id] = user.id
       redirect_to user_path(user.id)
--- spec/feature/root_page_spec.rb
+++ spec/feature/root_page_spec.rb
@@ -5,6 +5,38 @@ RSpec.describe 'root page',type: :system do
         it 'shows sign in form' do
             visit root_path
             expect(page).to have_content('Sign In')
+            expect(page).to have_field('username')
+            expect(page).to have_button('Proceed')
         end
+    end
+    describe 'login form' do
+      let(:user){ a = User.create!(username:'akshat') }
+      it 'redirects to root_path if user does not exists' do
+        visit root_path
+        fill_in 'username',with: 'nonexisting'
+        click_button 'Proceed'
+        fill_in 'username' ,with: 'wait'
+        expect(page.current_path).to eq('/')
+      end
+
+      it 'redirects to user_path if user exists also checks sign up' do
+          
+          visit root_path
+          click_link 'Sign Up'
+          fill_in 'username',with: 'akshat'
+          expect(page).to have_content('Sign Up')
+          click_button 'Proceed'
+          fill_in 'username',with: 'akshat'
+          click_button 'Proceed'
+          
+          expect(page.current_path).to eq(user_path(1))
+        end
+      it 'All Events link redirect to event_index_path' do
+        visit root_path
+
+        click_link 'All events'
+        expect(page).to have_content('Old Events')
+        expect(page).to have_content('Upcoming Events')
+      end
     end
 end
